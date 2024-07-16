95a3660964cbe3288f13a37e480231a392625ec0
aashishtyagi0802 <aashishtyagi0802@gmail.com>
2024-07-15 21:32:55 -0700

    this is third commit

--- Main.java
+++ Main.java
@@ -1,78 +1,13 @@
-/*
-import java.util.Scanner;
-
 class Main
 {
-public static void main(String[] args)
-{
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the number of Row: ");
-    int r=sc.nextInt();
-    System.out.println("Enter the number of Column:");
-    int c=sc.nextInt();
-    System.out.println("Now enter the Row and Column elements: ");
-    int [][] number=new int [r][c];
-    for(int i=0;i<r;i++)
+    public static void main(String []args)
     {
-        for(int j=0;j<c;j++)
-        {
-        number[i][j]=sc.nextInt();
-        }
-    }
-    for(int i=0;i<r;i++)
-    {
-        for(int j=0;j<c;j++)
-        {
-            System.out.print(number[i][j]+" ");
-        }
-        System.out.println();
-    }
-
-}
-}
-
-*/
-//**********************************************************************************************************************
-//Take a matrix as input from the user. Search for a given number X and print the indices at which it occurs.
-
-
-
-import java.util.*;
-class Main
-{
-
-    public static void main(String [] args)
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the row number: ");
-        int r=sc.nextInt();
-
-        System.out.println("Enter the column number: ");
-        int c=sc.nextInt();
-        int [][] number = new int [r][c];
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0; j<c;j++)
+        int i=0;
+        do
             {
-                number[i][j]=sc.nextInt();
-            }
-        }
-
-        System.out.println("Enter the value of X which need to found: ");
-        int x=sc.nextInt();
-
-        for(int i=0;i<r;i++)
-        {
-            for(int j=0;j<c; j++)
-            {
-                if(number[i][j]==x)
-                {
-                    System.out.println("The location of X is ("+ i+" , "+ j +" )");
-                }
-
-            }
-        }
-
+                System.out.println("do while loos");
+                i++;
+            }while(i==2);
 
     }
 }
